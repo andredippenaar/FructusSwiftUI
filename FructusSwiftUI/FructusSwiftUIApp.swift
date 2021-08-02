642918a224460ba00c2bded263a6eceb141ef8cd
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-08-02 10:19:33 +0200

    Initial Commit

---
+++ FructusSwiftUI/FructusSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusSwiftUIApp.swift
+//  FructusSwiftUI
+//
+//  Created by Andre Dippenaar on 2021/08/02.
+//
+
+import SwiftUI
+
+@main
+struct FructusSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
