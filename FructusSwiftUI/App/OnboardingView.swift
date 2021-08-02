cd5951809971a4ad1021d330f12ebd34edb7c6b3
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-08-02 10:29:10 +0200

    Set up folder structure

---
+++ FructusSwiftUI/App/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  OnboardingView
+//
+//  Created by Andre Dippenaar on 2021/08/02.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
