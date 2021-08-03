c0456cefb79aada5175adae6c48764f38200af38
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-08-03 15:09:31 +0200

    Complete Settings View Some refactoring for iPad

--- FructusSwiftUI/App/SettingsView.swift
+++ FructusSwiftUI/App/SettingsView.swift
@@ -13,6 +13,8 @@ struct SettingsView: View {
     
     @Environment(\.presentationMode) var presentationMode
     
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
+    
     // MARK: -  BODY -
     
     var body: some View {
@@ -40,7 +42,45 @@ struct SettingsView: View {
                     
                     // MARK: -  SECTION 2 -
                     
+                    GroupBox(label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")) {
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("If you wish, you can restart the application by toggling the switch in this box. That way it starts the onboarding process and you will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(Color(UIColor.tertiarySystemBackground)
+                                        .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous)))
+                    }
+                    
                     // MARK: -  SECTION 3 -
+                    
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
+                    ){
+                       SettingsRowView(name: "Developer", content: "Andre Dippenaar")
+                        SettingsRowView(name: "Designer", content: "Robert Patras")
+                        SettingsRowView(name: "Compatibility", content: "iOS 14")
+                        SettingsRowView(name: "Website", linkLabel: "SwiftUI Masterclass", linkDestination: "swiftuimasterclass.com")
+                        SettingsRowView(name: "Twitter", linkLabel: "@Andre_Dippenaar", linkDestination: "twitter.com/andre_dippenaar")
+                        SettingsRowView(name: "SwiftUI", content: "2.0")
+                        SettingsRowView(name: "Version", content: "1.1.0")
+                    }//:BOX
                    
                 }//: VSTACK
                 .navigationBarTitle(Text("Settings"), displayMode: .large)
