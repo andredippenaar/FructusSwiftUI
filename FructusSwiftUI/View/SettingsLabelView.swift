6465891298eafbfb4cb200e3f7248ecb6b8ff7af
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-08-03 12:19:01 +0200

    Complete Section 1 of Settings View

---
+++ FructusSwiftUI/View/SettingsLabelView.swift
@@ -0,0 +1,34 @@
+//
+//  SettingsLabelView.swift
+//  SettingsLabelView
+//
+//  Created by Andre Dippenaar on 2021/08/03.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    
+    // MARK: -  PROPERTIES -
+    
+    var labelText: String
+    var labelImage: String
+    
+    // MARK: -  BODY -
+    var body: some View {
+        HStack {
+            Text(labelText.uppercased()).fontWeight(.bold)
+            Spacer()
+            Image(systemName: labelImage)
+        }
+    }
+}
+
+// MARK: -  PROPERTIES -
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
